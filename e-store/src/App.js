8777a0d872ca6a9b80aa270b4fb258bc70c09b24
AaronRJ99 <80440300+AaronRJ99@users.noreply.github.com>
2024-01-26 22:50:31 +0800

    Project Entry

---
+++ e-store/src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+import Category from './components/Category';
+
+function App() {
+  const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState([])
+
+  React.useEffect(()=>{ 
+    fetch("http://localhost:3001/categories").then(response => response.json())
+    .then(data => {
+        console.log(data);
+        setCategories(data);
+    })
+  },[])
+
+  const handleCategoryClick = id => {
+    fetch("http://localhost:3001/products?catId=" + id)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setProducts(data);
+    })
+  }
+
+  const renderCategories = () => {
+    return categories.map( c =>
+      <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)}/>
+    );
+  }
+
+  const renderProducts = () => {
+    return products.map(p =>
+      <div>{p.title}</div>  
+    )
+  }
+
+  return (
+  <>
+  <header>My Store</header>
+
+  <section>
+    <nav>
+      {categories && renderCategories()}
+    </nav>
+    
+    <article>
+      <h1>Products</h1>
+      {products && renderProducts()}
+    </article>
+  </section>
+
+  <footer>
+    footer
+  </footer>
+  </>
+  );
+}
+
+export default App;
